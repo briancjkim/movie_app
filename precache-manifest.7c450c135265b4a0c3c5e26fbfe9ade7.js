b5e5ee8ea51c8b370be72bd76eeb923faaf9704f
brian <briancjkim91@gmail.com>
2018-11-28 23:10:59 +1100

    Updates

---
+++ precache-manifest.7c450c135265b4a0c3c5e26fbfe9ade7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "8882573b56cc4f71080a",
+    "url": "/movie_app/static/js/main.8882573b.chunk.js"
+  },
+  {
+    "revision": "0bee8e5dcb3c5e319809",
+    "url": "/movie_app/static/js/1.0bee8e5d.chunk.js"
+  },
+  {
+    "revision": "8882573b56cc4f71080a",
+    "url": "/movie_app/static/css/main.8fd5284d.chunk.css"
+  },
+  {
+    "revision": "db558e04d744387b4671a6bd2827fa12",
+    "url": "/movie_app/index.html"
+  }
+];
